12c31be0bd72cd50238eb1cf752481f1155d8a85
mbarcin <mbburdzy@gmail.com>
2019-07-17 14:12:05 +0200

    v1

--- break_trend.py
+++ break_trend.py
@@ -14,7 +14,7 @@ def best_fit_slope(xs, ys):
     #b = mean(ys) - m*mean(xs)
     return m #,b
 
-def table_id(year, df):
+def table_id(df, year):
     temp = df.loc[df.iloc[:, 0] == year].sort_values(by=[df.columns[0], df.columns[1]])
     return temp.iloc[:, 4].drop_duplicates().to_frame()
 
@@ -28,13 +28,20 @@ def max_year_month(df):
     max_year_month1 = int(max_year_month0.iloc[:, 1].max())  # ------------ Think about it
     return max_year, max_year_month1
 
+def slope_per_range(year=0):
+    return year
+
 def cumulative_slope_per_month(df, year):
-    pass
+    df1 = df.loc[df.iloc[:, 0] == year].sort_values(by=[df.columns[0], df.columns[1]])
+    for i in list(df1.iloc[:, 2].drop_duplicates()):
+        if len(df1.loc[df1.iloc[:, 2] == i]) >= 3:
+            #x = df1.loc[df1.iloc[:, 2] == i]
+            df_main = pd.merge(table_id(df, year), df1.loc[df1.iloc[:, 2] == i], on='id', how='left').fillna(0)
+            print(df_main)
+    #return x
 
-def slope_per_range():
-    pass
 
-
+print(cumulative_slope_per_month(get_data(), 2019))
 
 
 
